e86a87d2525f7e1b96e934c538bf403d5bb615e2
Kruthikesh <98465500+Kruthikesh@users.noreply.github.com>
2022-06-04 23:24:08 +0530

    Add files via upload

---
+++ search.js
@@ -0,0 +1,266 @@
+
+var input = document.getElementById("textbox_id");
+var inputValue = document.getElementById("textbox_id").value;
+var var1="iphone9 ";
+var v2="iphone 9X ";
+var v3="samsung universe 9 ";
+var v4="oppof19 ";
+var v5="huawei p30 ";
+var v6="macbook pro ";
+var v7="samsung galaxy book ";
+var v8=":microsoft surface laptop 4 ";
+var v9="infinix inbook ";
+var v10="hp pavilion 15-dk1056wm ";
+var v11="perfume oil ";
+var v12=":brown perfume ";
+var v13="fog scent xpressio perfume ";
+var v14="perfume oil ";
+var v15="eau de perfume spray ";
+var v16="hyaluronic acid serum ";
+var v17="tree oil ";
+var v18="toil free moisturizer ";
+var v19="skin beauty serum ";
+var v20="freckle treatment cream ";
+var v21="daal masoor ";
+var v22="elbow macaroni ";
+var v23=":orange essence food flavour ";
+var v24="cereals muesli fruit nuts ";
+var v25="gulab powder ";
+var v26="plant hanger for home ";
+var v27="flying wooden bird ";
+var v28="3D embellishment art lamp ";
+var v29="handcraft chinese style ";
+var v30="key holder";
+// console.log(var1);
+input.addEventListener("keypress", function() {
+  inputValue = document.getElementById("textbox_id").value;
+  inputValue=inputValue.toLowerCase()
+
+    // console.log(inputValue);
+
+    if(var1.includes(inputValue))
+    {
+      document.querySelector("#one").style.display="block";
+     }
+     else
+     {     document.querySelector("#one").style.display="none";
+    }
+    if(v2.includes(inputValue))
+    {
+      document.querySelector("#two").style.display="block";
+     }
+     else
+     {     document.querySelector("#two").style.display="none";
+    }
+    if(v3.includes(inputValue))
+    {
+      document.querySelector("#three").style.display="block";
+     }
+     else
+     {     document.querySelector("#three").style.display="none";
+    }
+    if(v4.includes(inputValue))
+    {
+      document.querySelector("#four").style.display="block";
+     }
+     else
+     {     document.querySelector("#four").style.display="none";
+    }
+    if(v5.includes(inputValue))
+    {
+      document.querySelector("#five").style.display="block";
+     }
+     else
+     {     document.querySelector("#five").style.display="none";
+    }
+
+    if(v6.includes(inputValue))
+    {
+      document.querySelector("#six").style.display="block";
+     }
+     else
+     {     document.querySelector("#six").style.display="none";
+    }
+    if(v7.includes(inputValue))
+    {
+      document.querySelector("#seven").style.display="block";
+     }
+     else
+     {     document.querySelector("#seven").style.display="none";
+    }
+    if(v8.includes(inputValue))
+    {
+      document.querySelector("#eight").style.display="block";
+     }
+     else
+     {     document.querySelector("#eight").style.display="none";
+    }
+
+    if(v9.includes(inputValue))
+    {
+      document.querySelector("#nine").style.display="block";
+     }
+     else
+     {     document.querySelector("#nine").style.display="none";
+    }
+    if(v10.includes(inputValue))
+    {
+      document.querySelector("#ten").style.display="block";
+     }
+     else
+     {     document.querySelector("#ten").style.display="none";
+    }
+    if(v11.includes(inputValue))
+    {
+      document.querySelector("#eleven").style.display="block";
+     }
+     else
+     {     document.querySelector("#eleven").style.display="none";
+    }
+    if(v12.includes(inputValue))
+    {
+      document.querySelector("#twelve").style.display="block";
+     }
+     else
+     {     document.querySelector("#twelve").style.display="none";
+    }
+    if(v13.includes(inputValue))
+    {
+      document.querySelector("#thirteen").style.display="block";
+     }
+     else
+     {     document.querySelector("#thirteen").style.display="none";
+    }
+    if(v14.includes(inputValue))
+    {
+      document.querySelector("#fourteen").style.display="block";
+     }
+     else
+     {     document.querySelector("#fourteen").style.display="none";
+    }
+    if(v15.includes(inputValue))
+    {
+      document.querySelector("#fifteen").style.display="block";
+     }
+     else
+     {     document.querySelector("#fifteen").style.display="none";
+    }
+    if(v16.includes(inputValue))
+    {
+      document.querySelector("#sixteen").style.display="block";
+     }
+     else
+     {     document.querySelector("#sixteen").style.display="none";
+    }
+    if(v17.includes(inputValue))
+    {
+      document.querySelector("#seventeen").style.display="block";
+     }
+     else
+     {     document.querySelector("#seventeen").style.display="none";
+    }
+    if(v18.includes(inputValue))
+    {
+      document.querySelector("#eighteen").style.display="block";
+     }
+     else
+     {     document.querySelector("#eighteen").style.display="none";
+    }
+    if(v19.includes(inputValue))
+    {
+      document.querySelector("#nineteen").style.display="block";
+     }
+     else
+     {     document.querySelector("#nineteen").style.display="none";
+    }
+    if(v20.includes(inputValue))
+    {
+      document.querySelector("#twenty").style.display="block";
+     }
+     else
+     {     document.querySelector("#twenty").style.display="none";
+    }
+    if(v21.includes(inputValue))
+    {
+      document.querySelector("#twentyone").style.display="block";
+     }
+     else
+     {     document.querySelector("#twentyone").style.display="none";
+    }
+    if(v22.includes(inputValue))
+    {
+      document.querySelector("#twentytwo").style.display="block";
+     }
+     else
+     {     document.querySelector("#twentytwo").style.display="none";
+    }
+    if(v23.includes(inputValue))
+    {
+      document.querySelector("#twentythree").style.display="block";
+     }
+     else
+     {     document.querySelector("#twentythree").style.display="none";
+    }
+    if(v24.includes(inputValue))
+    {
+      document.querySelector("#twentyfour").style.display="block";
+     }
+     else
+     {     document.querySelector("#twentyfour").style.display="none";
+    }
+    if(v25.includes(inputValue))
+    {
+      document.querySelector("#twentyfive").style.display="block";
+     }
+     else
+     {     document.querySelector("#twentyfive").style.display="none";
+    }
+    if(v26.includes(inputValue))
+    {
+      document.querySelector("#twentysix").style.display="block";
+     }
+     else
+     {     document.querySelector("#twentysix").style.display="none";
+    }
+    if(v27.includes(inputValue))
+    {
+      document.querySelector("#twentyseven").style.display="block";
+     }
+     else
+     {     document.querySelector("#twentyseven").style.display="none";
+    }
+    if(v28.includes(inputValue))
+    {
+      document.querySelector("#twentyeight").style.display="block";
+     }
+     else
+     {     document.querySelector("#twentyeight").style.display="none";
+    }
+    if(v29.includes(inputValue))
+    {
+      document.querySelector("#twentynine").style.display="block";
+     }
+     else
+     {     document.querySelector("#twentynine").style.display="none";
+    }
+    if(v30.includes(inputValue))
+    {
+      document.querySelector("#thirty").style.display="block";
+     }
+     else
+     {     document.querySelector("#thirty").style.display="none";
+    }
+   });
+
+//   var n=30;
+//   for(var i =1;i<=n;i++)
+//   {
+//     if(document.getElementsById(i)==inputValue)
+//     {
+//
+//     }
+//   }
+//
+//   // do something with it
+//
+// document.querySelector("#second").style.display="none";
